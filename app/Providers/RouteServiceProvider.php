632934d53e6ff45f04db93893b765a1ed119a77b
Jessica Castelli <sitelifters@gmail.com>
2020-08-28 16:26:09 -0400

    Explicitly bind Product model to routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use App\Product;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -33,6 +34,8 @@ class RouteServiceProvider extends ServiceProvider
         //
 
         parent::boot();
+
+        Route::model('product', Product::class);
     }
 
     /**
